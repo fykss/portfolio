84c8d9e144bfcec91b1267b8d031cec3a36dadd9
Влад Летун <fyks1649@gmail.com>
2019-06-19 14:17:05 +0300

    Updates

---
+++ precache-manifest.f563007d577c914b8c3e9b1813be6e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5685c8f2d87da97cdcb5dad9bbf7cc25",
+    "url": "/fykss/portfolio/index.html"
+  },
+  {
+    "revision": "328b6e429aaba5154c81",
+    "url": "/fykss/portfolio/static/css/main.16a131b5.chunk.css"
+  },
+  {
+    "revision": "521c3192465ed30cca66",
+    "url": "/fykss/portfolio/static/js/2.db5a47f6.chunk.js"
+  },
+  {
+    "revision": "328b6e429aaba5154c81",
+    "url": "/fykss/portfolio/static/js/main.429af66f.chunk.js"
+  },
+  {
+    "revision": "1ffbb60b6615876f494c",
+    "url": "/fykss/portfolio/static/js/runtime~main.63d87a2f.js"
+  },
+  {
+    "revision": "afa32bb727bd149a9729c85eed6b466b",
+    "url": "/fykss/portfolio/static/media/photo.afa32bb7.png"
+  }
+]);
